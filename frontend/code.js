88f5a7b936d6bfbffb15d85fa6b134edf5e483d7
rob <robertsilveiracrust@gmail.com>
2021-04-18 16:10:45 -0300

    close menu

--- frontend/code.js
+++ frontend/code.js
@@ -1,7 +1,16 @@
 const openMenu = document.getElementById('openMenu')
 const dropdown = document.getElementById('navDemo')
+const menuIcon = document.querySelector('#openMenu i')
+const menuText = document.querySelector('#openMenu span')
 
 openMenu.addEventListener('click', () => {
-    console.log(dropdown)
     dropdown.classList.toggle('w3-hide');
+    menuIcon.classList.toggle('fa-bars');
+    menuIcon.classList.toggle('fa-close');
+    if(menuText.innerText == 'MENU') {
+        menuText.innerText = 'FECHAR'
+    }
+    else {
+        menuText.innerText = 'MENU'
+    }
 })
